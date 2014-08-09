29a34016499e70ca42adfe7f7e5f0859a1366556
littler.chicken <littler.chicken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-09 01:02:59 +0000

    Adding to the repo for real!
    git-svn-id: https://plugins.svn.wordpress.org/send-images-rss/trunk@962822 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/class-sendimagesrss.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Send Images to RSS
+ *
+ * @package   SendImagesRSS
+ * @author    Robin Cornett <hello@robincornett.com>
+ * @author    Gary Jones <gary@garyjones.co.uk>
+ * @link      https://github.com/robincornett/send-images-rss
+ * @copyright 2014 Robin Cornett
+ * @license   GPL-2.0+
+ */
+
+/**
+ * Main plugin class.
+ *
+ * @package SendImagesRSS
+ */
+class SendImagesRSS {
+
+	public $gallery_stripper;
+	public $feed_fixer;
+	public $settings;
+
+	/**
+	 * Inject dependencies.
+	 *
+	 * @since 2.4.0
+	 */
+	public function __construct( $gallery_stripper, $feed_fixer, $settings ) {
+		$this->gallery_stripper = $gallery_stripper;
+		$this->feed_fixer       = $feed_fixer;
+		$this->settings         = $settings;
+	}
+
+	/**
+	 * Set up hooks.
+	 *
+	 * @since 2.4.0
+	 */
+	public function run() {
+		add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
+		add_action( 'init', array( $this, 'init' ) );
+		add_action( 'admin_init', array( $this->settings, 'register_settings' ) );
+		add_action( 'load-options-media.php', array( $this->settings, 'help' ) );
+		add_action( 'admin_notices', array( $this->settings, 'error_message' ) );
+		add_action( 'template_redirect', array( $this, 'fix_feed' ) );
+	}
+
+	/**
+	 * Set up text domain for translations
+	 *
+	 * @since 2.4.0
+	 */
+	public function load_textdomain() {
+		load_plugin_textdomain( 'send-images-rss', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Add a feed-specific image size, and custom feed.
+	 *
+	 * @since 2.0.0
+	 */
+	public function init() {
+		$simplify = get_option( 'sendimagesrss_simplify_feed' );
+		$image_width = esc_attr( get_option( 'sendimagesrss_image_size', '560' ) );
+
+		if ( ! $simplify ) {
+			add_image_size( 'mailchimp', intval( $image_width ) );
+		}
+
+		// Add a new feed, but tell WP to treat it as a standard RSS2 feed
+		// We do this so the output is the same by default, but we can use
+		// the different querystring value to conditionally apply the fixes.
+		$alt_feed = get_option( 'sendimagesrss_alternate_feed' );
+		if ( $alt_feed ) {
+			add_feed( 'email', 'do_feed_rss2' );
+		}
+	}
+
+	/**
+	 * Choose which feeds to fix.
+	 *
+	 * @since 2.3.0
+	 *
+	 * @return null Return early if not a feed.
+	 */
+	public function fix_feed() {
+		if ( ! is_feed() ) {
+			return;
+		}
+
+		elseif ( is_feed() ) {
+			add_filter( 'the_content', array( $this->gallery_stripper, 'strip' ), 19 );
+
+			$simplify = get_option( 'sendimagesrss_simplify_feed' );
+			$alt_feed = get_option( 'sendimagesrss_alternate_feed' );
+
+			if ( ! $simplify && ( ( $alt_feed && is_feed( 'email' ) ) || ! $alt_feed ) ) {
+				add_filter( 'the_content', array( $this->feed_fixer, 'fix' ), 20 );
+			}
+		}
+	}
+}
---
+++ send-images-rss.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Send Images to RSS
+ *
+ * @package           SendImagesRSS
+ * @author            Robin Cornett
+ * @author            Gary Jones <gary@garyjones.co.uk>
+ * @link              https://github.com/robincornett/send-images-rss
+ * @copyright         2014 Robin Cornett
+ * @license           GPL-2.0+
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Send Images to RSS
+ * Plugin URI:        https://github.com/robincornett/send-images-rss
+ * Description:       Makes your RSS emails look more like your website by converting overly large images and galleries to an email friendly format. Built with MailChimp in mind.
+ * Version:           2.4.1
+ * Author:            Robin Cornett
+ * Author URI:        http://robincornett.com
+ * Text Domain:       send-images-rss
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/robincornett/send-images-rss
+ * GitHub Branch:     master
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+// Include classes
+require plugin_dir_path( __FILE__ ) . 'includes/class-sendimagesrss.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-sendimagesrss-feed-fixer.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-sendimagesrss-strip-gallery.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-sendimagesrss-settings.php';
+
+// Instantiate dependent classes
+$sendimagesrss_strip_gallery = new SendImagesRSS_Strip_Gallery;
+$sendimagesrss_feed_fixer = new SendImagesRSS_Feed_Fixer;
+$sendimagesrss_settings = new SendImagesRSS_Settings;
+
+// Instantiate main class and pass in dependencies
+$sendimagesrss = new SendImagesRSS(
+	$sendimagesrss_strip_gallery,
+	$sendimagesrss_feed_fixer,
+	$sendimagesrss_settings
+);
+
+// Run the plugin
+$sendimagesrss->run();
